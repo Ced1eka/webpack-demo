99f43c986d1ff4a2b1175bba133d77d2d7621494
Ced <cedieka2@gmail.com>
2024-04-13 15:23:28 +0000

    webpack how to guide

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    index: './src/index.js',
-    print: './src/print.js',
+    index: "./src/index.js",
+    print: "./src/print.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
 };
